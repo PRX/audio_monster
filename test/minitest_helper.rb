6bdb6461feb8bbb44141c3c82681bbf9e629684a
Andrew Kuklewicz <andrew@beginsinwonder.com>
2015-01-09 15:21:22 -0500

    add helper methods for files

--- test/minitest_helper.rb
+++ test/minitest_helper.rb
@@ -5,3 +5,11 @@ require 'minitest'
 require 'minitest/autorun'
 require 'minitest/spec'
 require 'minitest/mock'
+
+def out_file(o)
+  File.expand_path(File.dirname(__FILE__) + '/tmp/' + o)
+end
+
+def in_file(i)
+  File.expand_path(File.dirname(__FILE__) + '/files/' + i)
+end
