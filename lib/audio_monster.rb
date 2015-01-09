c2563e715aecca9909121bca33b44fbfaeb6e5ad
Andrew Kuklewicz <andrew@beginsinwonder.com>
2015-01-09 12:55:18 -0500

    add in monster to module and delegate calls to it

--- lib/audio_monster.rb
+++ lib/audio_monster.rb
@@ -2,7 +2,17 @@
 
 require 'audio_monster/version'
 require 'audio_monster/configuration'
+require 'audio_monster/monster'
 
 module AudioMonster
   extend Configuration
+
+  def self.monster
+    @_monster ||= AudioMonster::Monster.new
+  end
+
+  # delegate to the monster inside
+  def self.method_missing(method, *args, &block)
+    monster.send(method, *args)
+  end
 end
--- test/audio_monster_test.rb
+++ test/audio_monster_test.rb
@@ -3,11 +3,18 @@ require 'minitest_helper'
 describe AudioMonster do
 
   it 'has a version number' do
-    ::AudioMonster::VERSION.wont_be_nil
+    AudioMonster::VERSION.wont_be_nil
   end
 
   it 'has configuration options' do
     AudioMonster.options.wont_be_nil
   end
 
+  it 'delegates methods to monster' do
+    AudioMonster.flac.must_equal 'flac'
+  end
+
+  it 'returns an instance of monster' do
+    AudioMonster.monster.wont_be_nil
+  end
 end
